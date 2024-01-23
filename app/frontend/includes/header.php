e6a7bff72b631241df194254af5e1f731de25b95
jepp8530 <jepp8530@mail.hansenberg.dk>
2024-01-23 13:31:01 +0100

    Style insert and fix

--- app/frontend/includes/header.php
+++ app/frontend/includes/header.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo FRONTEND_ASSET . 'css/style.css'; ?>">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
@@ -15,17 +16,11 @@
 
   <link rel="stylesheet" href="<?php //echo FRONTEND_ASSET . 'css/profile.css'; ?>">
 -->
-  <style>
-    .fakeimg {
-      height: 200px;
-      background: #aaa;
-    }
-  </style>
 </head>
 
 <body>
 
-<div class="jumbotron text-center" style="margin-bottom:0">
+<div class="jumbotron text-center header" style="margin-bottom:0">
   <h1>My First <?php appName(); ?></h1>
   <p>Resize this responsive page to see the effect!</p>
   <?php if ($user->isLoggedIn()) : ?>
